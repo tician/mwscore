6d6f7b6888412b5b2c5bfa537ef61cc9788c7600
tician <tician@gmail.com>
2015-07-03 17:53:25 -0400

    renamed and changed license

--- transponder/firmware/yetis_ltb_v0/yetis_ltb_v0.cpp
+++ transponder/firmware/yetis_ltb_v0/yetis_ltb_v0.cpp
@@ -1,22 +1,35 @@
 /*
  *******************************************************************************
- *  Copyright (c) 2012, 2013, 2014, 2015 Matthew Paulishen. All rights reserved.
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *  
+ * YETIS ATtiny45/85 I2C Device - Lights and Target Board
+ *******************************************************************************
+ * Copyright (c) 2015, Matthew Paulishen.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************
  */
+
 
 // sudo avrdude -c usbasp -P USB -p t85 -v -U flash:w:./yetis_itb_v0.hex
 
@@ -125,7 +138,7 @@ uint8_t EEMEM eep_hit_standoffH = (1000>>8)&0xFF;	// [ms]
 uint8_t EEMEM eep_dph_value = 1;	// [damage per hit]
 
 uint8_t EEMEM eep_fsr_sdph = 5;	// Sequential ADC Detections Per Hit
-uint8_t EEMEM eep_fsr_threshold = 230;	// ADC Threshold for Detection
+uint8_t EEMEM eep_fsr_threshold = 60;	// ADC Threshold for Detection
 
 uint8_t EEMEM eep_leds_im_hit = (yetisI2Cdevs::LEDS_ALTERNATING | yetisI2Cdevs::FREQ_8_000Hz);
 uint8_t EEMEM eep_leds_other_hit = (yetisI2Cdevs::LEDS_SYNCHRONOUS | yetisI2Cdevs::FREQ_8_000Hz);
@@ -353,9 +366,6 @@ void loop()
 	uint16_t fsr = 255;
 	uint8_t hit_detect = 0;
 	uint16_t damage = 0;
-
-// Set device address
-//	usiTwiSlaveInit(x00_table[3]);
 
 	while(1)
 	{
@@ -688,8 +698,8 @@ void configTimer0_millis(void)
 
 ISR(TIMER0_OVF_vect)
 {
-	uint8_t sreg = SREG;
-	cli();
+//	uint8_t sreg = SREG;
+//	cli();
 
 	millis_counter++;
 	I2C_Stop_Check();
@@ -724,7 +734,7 @@ ISR(TIMER0_OVF_vect)
 		millis_other_hit--;
 	}
 
-	SREG = sreg;
+//	SREG = sreg;
 }
 
 uint32_t millis(void)
